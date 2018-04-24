d37a1ebfa6863de162a77daae04ea648735ac3ee
CESI Exia <CESI Exia@Thomas-VAIO>
2018-04-24 09:48:26 +0200

    Phase 2

---
+++ src/jpu2016/dogfight/view/DogfightView.java
@@ -0,0 +1,25 @@
+/*package jpu2016.dogfight.view;
+
+import java.util.Observable;
+
+public class DogfightView {
+
+	public DogfightView(IOrderPerformer orderPerformer, IDogfightModel dogfightModel, Observable observable) {
+		// TODO Auto-generated constructor stub
+		
+		
+	}
+	
+	private void run() {
+		
+	}
+	
+	public void displayMessage(String message) {
+		
+	}
+	
+	public void closeAll() {
+		
+	}
+	
+}*/
